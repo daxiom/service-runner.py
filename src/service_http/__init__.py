a8552fdeeec303e1066b7cd97774fa562e9caeb9
thorwolpert <thor@wolpert.ca>
2022-01-08 21:36:03 -0800

    allow non-ce alternate-data as well parse for wrapped ce messages

--- src/service_http/__init__.py
+++ src/service_http/__init__.py
@@ -1,13 +1,14 @@
 """service template package."""
+import base64
 import json
 import os
 import time
 from datetime import datetime, timezone
 from http import HTTPStatus
-from typing import Callable, Final
+from typing import Callable, Final, MutableMapping
 
 from flask import Flask, current_app, request, _app_ctx_stack
-from simple_cloudevent import from_queue_message
+from simple_cloudevent import CloudEventVersionException, InvalidCloudEventError, from_queue_message
 
 from .config import Config
 from .logging import structured_log
@@ -27,7 +28,6 @@ def get_callback() -> Callable:
 def set_callback(cb: Callable):
     current_app.config.setdefault(CALL_BACK_NAME, cb)
 
-
 def create_app(env: str = None, call_back: Callable = default_call_back):
     app = Flask(__name__)
     app.config.from_object(Config())
@@ -42,14 +42,32 @@ def create_app(env: str = None, call_back: Callable = default_call_back):
         app.logger.info('got a POST message')
         structured_log(request=request, severity='NOTICE', message='a POST')
 
-        call_back = get_callback()
+        if not (request_data := request.data):
+            app.logger.error('no data in POST')
+            # return OK so that the message is not re-queued
+            return HTTPStatus.OK
+        
+        if not (call_back := get_callback()):
+            app.logger.error('no callback set')
+            # return a non 200 so that the message is requeued
+            # TODO: figure out if this should Panic or not
+            return HTTPStatus.SERVICE_UNAVAILABLE
+        
+        ce = alt = None
+        try:
+            message_bytes = request_data
+            if isinstance(msg_dict := json.loads(request_data.decode('UTF8')), MutableMapping):
+                if msg_dict.get('subscription') and msg_dict.get('message'):
+                    base64_bytes = msg_dict['message']['data']
+                    message_bytes = base64.b64decode(base64_bytes)
 
-        if request.data and \
-           (ce := from_queue_message(request.data)
-        ):
-            app.logger.info('call back for ce:{ce}')
-            rc = call_back(ce)
-            app.logger.debug('call back returned:{rc.status_code} for ce:{ce}')
+            ce = from_queue_message(message_bytes)
+        except (CloudEventVersionException, InvalidCloudEventError, ValueError) as e:
+            alt = request_data
+    
+        app.logger.info('call back for ce:{ce}, alt {alt}'.format(ce=ce, alt=alt))
+        rc = call_back(ce, alt)
+        app.logger.debug('call back returned:{rc} for ce:{ce}, alt {alt}'.format(rc=rc, ce=ce, alt=alt))
 
         return {}, rc if ('rc' in locals()) else 200
 
